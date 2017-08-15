8f7772202488208af77c62ef071b858deaad2558
robinHurtado <robin_aven94@hotmail.com>
2017-08-15 15:50:24 -0500

    Add logic to show every text that is corresponding to every audio

--- public/js/dictame.js
+++ public/js/dictame.js
@@ -1,20 +1,28 @@
 window.onload = function() {
-  // onClick event for "Enviar" button
-  document.getElementById('send').onclick = function() {
-    document.getElementById('textArea').hidden = true;   // hide the textarea
-    textAreaVal = document.getElementById('textArea').value;  // get the value entered by the user
-    document.getElementById('userText').innerHTML = textAreaVal; // set the value to a hidden span
-    document.getElementById('userText').hidden = false;  // show the span containing the user text
-    document.getElementById('originText').hidden = false; // show original text
-    document.getElementById('send').hidden = true; // hide the "Solucón" button
-    document.getElementById('back').hidden = false; // show the "Atras" button
-    document.getElementById('yourText').hidden = false; // show label
+
+	var allTexts = null;
+	var audiosList = ["./audios/dictation1.mp3","./audios/dictation2.mp3","./audios/dictation3.mp3"];
+	var audiosTitle = ["Bear breaks into house","10 New Year's Resolutions You Probably Won't Keep","Strange Things Happened at Halloween"];
+	var indx = -1;
+
+	// onClick event for "Enviar" button
+	document.getElementById('send').onclick = function() {
+	    document.getElementById('textArea').hidden = true;   // hide the textarea
+	    textAreaVal = document.getElementById('textArea').value;  // get the value entered by the user
+	    document.getElementById('userText').innerHTML = textAreaVal; // set the value to a hidden span
+	    document.getElementById('userText').hidden = false;  // show the span containing the user text
+	    document.getElementById('originText').hidden = false; // show original text
+	    document.getElementById('send').hidden = true; // hide the "Solucón" button
+	    document.getElementById('back').hidden = false; // show the "Atras" button
+	    document.getElementById('yourText').hidden = false; // show label
+	}
+
+      // get the original texts of the audios 
     fetch("js/textos.json")
     .then(response => response.json())
     .then(texts => {
-    	document.getElementById('originText').innerHTML = texts.text1;	
-    })
-  }
+    	allTexts = texts;
+    });
 
   // onClick event for "Atras" button
   document.getElementById('back').onclick = function(){
@@ -27,17 +35,14 @@ window.onload = function() {
     document.getElementById('back').hidden = true; // hide the "Solucón" button
   }
 
-  var audiosList = ["./audios/dictation1.mp3","./audios/dictation2.mp3","./audios/dictation3.mp3"];
-  var audiosTitle = ["Bear breaks into house","10 New Year's Resolutions You Probably Won't Keep","Strange Things Happened at Halloween"];
-  var indx = -1;
-  
-  document.getElementById('forward').onclick = function(){
-  	indx++;
-  	if (indx >= audiosList.length) {
-  		indx = 0;
-  	}
-  	document.getElementsByTagName("audio")[0].src = audiosList[indx];
-  	document.getElementById('h4title').firstChild.textContent = audiosTitle[indx];
-  }
-
+	// event when click forward arrow  
+	document.getElementById('forward').onclick = function(){
+		indx++;
+		if (indx >= audiosList.length) {
+			indx = 0;
+		}
+		document.getElementsByTagName("audio")[0].src = audiosList[indx];
+		document.getElementById('h4title').firstChild.textContent = audiosTitle[indx];
+		document.getElementById('originText').innerHTML = allTexts["text"+indx];
+	}
 }//finish function
