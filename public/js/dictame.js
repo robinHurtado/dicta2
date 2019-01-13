d90491a63fcbce26cd06994d689108d49efc1bd7
robinHurtado <robin_aven94@hotmail.com>
2019-01-12 21:38:49 -0500

    Adjust experience when changing audio

--- public/js/dictame.js
+++ public/js/dictame.js
@@ -1,16 +1,19 @@
 window.onload = function() {
-
 	var allTexts = null;
-	var audiosList = ["./audios/dictation1.mp3","./audios/dictation2.mp3","./audios/dictation3.mp3"];
-	var audiosTitle = ["Bear breaks into house","10 New Year's Resolutions You Probably Won't Keep","Strange Things Happened at Halloween"];
+	var audiosList = [
+		"./audios/dictation1.mp3",
+		"./audios/dictation2.mp3",
+		"./audios/dictation3.mp3"
+	];
+	var audiosTitle = [
+		"Bear breaks into house",
+		"10 New Year's Resolutions You Probably Won't Keep",
+		"Strange Things Happened at Halloween"
+	];
 	var indx = -1;
 
 	// onClick event for "Enviar" button
 	document.getElementById('send').onclick = function() {
-	    if (document.getElementById('originText').innerText === ""){
-	    	Materialize.toast('Con la flecha > sleccionas un texto!', 4000)
-	    	return false;
-	    }
 	    document.getElementById('textArea').hidden = true;   // hide the textarea
 	    textAreaVal = document.getElementById('textArea').value;  // get the value entered by the user
 	    document.getElementById('userText').innerHTML = textAreaVal; // set the value to a hidden span
@@ -18,7 +21,8 @@ window.onload = function() {
 	    document.getElementById('originText').hidden = false; // show original text
 	    document.getElementById('send').hidden = true; // hide the "Solucón" button
 	    document.getElementById('back').hidden = false; // show the "Atras" button
-	    document.getElementById('yourText').hidden = false; // show label
+		document.getElementById('yourText').hidden = false; // show label
+		document.getElementById('originText').innerHTML = allTexts["text"+indx];
 	}
 
 	// get the original texts of the audios 
@@ -26,18 +30,22 @@ window.onload = function() {
     .then(response => response.json())
     .then(texts => {
     	allTexts = texts;
-    });
+	});
+	
+	const handleElements = () => {
+		document.getElementById('userText').hidden = true; // hide the span containing the user text
+		document.getElementById('textArea').hidden = false; // show the textarea
+		document.getElementById('originText').hidden = true; // hide original text
+		document.getElementById('yourText').hidden = true; // hide label
+		document.getElementById('back').hidden = true; // hide the "Solucón" button
+		document.getElementById('send').hidden = false; // show the "Solucón" button
+	}
 
-  // onClick event for "Atras" button
-  document.getElementById('back').onclick = function(){
-    document.getElementById('textArea').value = "";
-    document.getElementById('textArea').hidden = false;   // show the textarea
-    document.getElementById('userText').hidden = true;  // hide the span containing the user text
-    document.getElementById('originText').hidden = true; // hide original text
-    document.getElementById('yourText').hidden = true; // hide label
-    document.getElementById('send').hidden = false; // show the "Solucón" button
-    document.getElementById('back').hidden = true; // hide the "Solucón" button
-  }
+	// onClick event for "Atras" button
+	document.getElementById('back').onclick = function(){
+		document.getElementById('textArea').value = "";
+		handleElements();
+	}
 
 	// fires event when click forward arrow  
 	document.getElementById('forward').onclick = function(){
@@ -45,8 +53,9 @@ window.onload = function() {
 		if (indx >= audiosList.length) {
 			indx = 0; // reset index to access to first position
 		}
-		document.getElementsByTagName("audio")[0].src = audiosList[indx]; // change audio on every click forward
+		// change audio on every click forward
+		document.getElementsByTagName("audio")[0].src = audiosList[indx];
 		document.getElementById('h4title').firstChild.textContent = audiosTitle[indx];
-		document.getElementById('originText').innerHTML = allTexts["text"+indx];
+		handleElements();
 	}
-}//finish function
+}
